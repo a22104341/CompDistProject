6441032c4c1964c4de795cfacdb5fcaecfcb58f9
alex-a22103429 <a22103429@alunos.ulht.pt>
2023-10-26 20:26:04 +0100

    primeiro

---
+++ src/Sombrinha.java
@@ -0,0 +1,24 @@
+public class Sombrinha {
+    private int id;
+    private int maxUsers;
+    private char praia;
+
+    public Sombrinha(int id, int maxUsers, char praia) {
+        this.id = id;
+        this.maxUsers = maxUsers;
+        this.praia = praia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getMaxUsers() {
+        return maxUsers;
+    }
+
+    public char getPraia() {
+        return praia;
+    }
+
+}
